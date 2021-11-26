cb42b0d6643c979085dfd80a34ebee2c39d77f37
dai-guu <daiguu2021@gmail.com>
2021-11-26 04:45:24 +0000

    [Finish]コード整理、レイアウト

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_20_095101) do
+ActiveRecord::Schema.define(version: 2021_11_26_002135) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -61,15 +61,6 @@ ActiveRecord::Schema.define(version: 2021_11_20_095101) do
     t.text "hashbody"
     t.text "body"
     t.string "category"
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "image_id"
-    t.text "introduction"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "relationships", force: :cascade do |t|
